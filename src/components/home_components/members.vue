3c059bbdd2e22295668a16f554857ccfc5a4ab4a
xavhl <50361174+xavhl@users.noreply.github.com>
2020-10-10 16:02:58 +0800

    Update members.vue

--- src/components/home_components/members.vue
+++ src/components/home_components/members.vue
@@ -16,7 +16,7 @@
       <b-collapse id="collapse-2">
         <b-card>
           <b-card-text>
-            {{contact.info}}
+            {{contact.intro}}
           </b-card-text>
         </b-card>
       </b-collapse>
@@ -33,49 +33,49 @@ export default {
                     id: 1,
                     name: 'Ankolika',
                     img: 'https://i.imgur.com/p7mWrrs.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Reading books, watching shows!',
                 },
                 {
                     id: 2,
                     name: 'Eugene',
                     img: 'https://i.imgur.com/tKEJq3B.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'I like playing indie games, and watching movies!',
                 },
                 {
                     id: 3,
                     name: 'Ilias',
                     img: 'https://i.imgur.com/eWOoYcv.png',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'History, Poetry, and a little bit of Machine Learning are my cup of tea :D',
                 },
                 {
                     id: 4,
                     name: 'Leon',
                     img: 'https://i.imgur.com/0xFprG3.png',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Robotics lover, Frontend engineer/learner, avid gamer\nLoves bringing virtual, unrealistic ideas to the ground through software and hardware engineering',
                 },
                 {
                     id: 4,
                     name: 'Luke',
                     img: 'https://i.imgur.com/Gi4ECTp.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Coding and Hiking, two of my favorite things',
                 },
                 {
                     id: 6,
                     name: 'Michelle',
                     img: 'https://i.imgur.com/nYImXq8.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'I like live music and stand-up comedy',
                 },
                 {
                     id: 7,
                     name: 'Alan',
                     img: 'https://i.imgur.com/CTvs13f.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'A newbie in tech, likes to brainstorm ideas a lot',
                 },
                 {
                     id: 8,
                     name: 'Rishabh',
                     img: 'https://i.imgur.com/FiCHl0M.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Constantly challenging myself to learn and grow. Passionate Coder and an amature Guitarist',
                 },
                 {
                     id: 9,
@@ -87,31 +87,31 @@ export default {
                     id: 10,
                     name: 'Shawn',
                     img: 'https://i.imgur.com/9SAYR6s.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Professional incel',
                 },
                 {
                     id: 11,
                     name: 'Su Jin',
                     img: 'https://i.imgur.com/DGG504Q.png',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'I always strive for improvement, but not perfection',
                 },
                 {
                     id: 12,
                     name: 'Theresa',
                     img: 'https://i.imgur.com/A8U3bY0.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Tech gadgets, productivity apps, cats and coffee!',
                 },
                 {
                     id: 13,
                     name: 'Varun',
                     img: 'https://i.imgur.com/N6jaKDu.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'Cricket. Gamer. Beleiver',
                 },
                 {
                     id: 14,
                     name: 'Xavier',
                     img: 'https://i.imgur.com/S01Uo1g.jpg',
-                    intro: 'Hello, My name is Ryan yen~ nice to meet you!!',
+                    intro: 'A novice delving into the realm of deep learning',
                 },
 
             ]
